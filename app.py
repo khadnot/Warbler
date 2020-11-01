257cc190fb97f44166fcf6e26c29df43ff595427
Kenn Hadnot <k_hadnot@yahoo.com>
2020-10-31 18:27:17 -0700

    added user location, bio, and header image

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
 
@@ -117,7 +117,7 @@ def logout():
     if CURR_USER_KEY in session:
         do_logout()
         flash("GoodBye!", "info")
-        return redirect('/')
+        return redirect('/login')
 
 
 ##############################################################################
