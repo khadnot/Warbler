3fe0bf314c07363f24bfe03253bff89ea0d5399b
Kenn Hadnot <k_hadnot@yahoo.com>
2020-11-01 16:36:55 -0800

    added location, header, and bio to user profile

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
-from forms import UserAddForm, LoginForm, MessageForm
+from forms import UserAddForm, LoginForm, MessageForm, UserEditForm
 from models import db, connect_db, User, Message
 
 CURR_USER_KEY = "curr_user"
@@ -215,7 +215,11 @@ def stop_following(follow_id):
 def profile():
     """Update profile for current user."""
 
-    # IMPLEMENT THIS
+    form = UserEditForm()
+
+    return render_template('home.html', form=form)
+
+
 
 
 @app.route('/users/delete', methods=["POST"])
--- forms.py
+++ forms.py
@@ -18,8 +18,19 @@ class UserAddForm(FlaskForm):
     image_url = StringField('(Optional) Image URL')
 
 
+
 class LoginForm(FlaskForm):
     """Login form."""
 
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6)])
+
+class UserEditForm(FlaskForm):
+    """ Form for editing user."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    bio = TextAreaField('(Optional) Text')
+    image_url = StringField('(Optional) Image URL')
+    header_image_url = StringField('(Option) Image URL')
