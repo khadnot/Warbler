94da5cff46b5497c8ebf04424b447d1a86b6f73a
Kenn Hadnot <k_hadnot@yahoo.com>
2020-11-06 11:40:20 -0800

    added tests to test_user_model.py

--- test_user_model.py
+++ test_user_model.py
@@ -8,7 +8,7 @@
 import os
 from unittest import TestCase
 
-from models import db, User, Message, Follows
+from models import db, User, Message, Follows, Likes
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -38,21 +38,152 @@ class UserModelTestCase(TestCase):
         User.query.delete()
         Message.query.delete()
         Follows.query.delete()
+        Likes.query.delete()
 
         self.client = app.test_client()
 
     def test_user_model(self):
         """Does basic model work?"""
 
-        u = User(
+        user = User(
             email="test@test.com",
             username="testuser",
             password="HASHED_PASSWORD"
         )
 
-        db.session.add(u)
+        db.session.add(user)
         db.session.commit()
 
-        # User should have no messages & no followers
-        self.assertEqual(len(u.messages), 0)
-        self.assertEqual(len(u.followers), 0)
+        # User should have no messages, followers, or likes
+        self.assertEqual(len(user.messages), 0)
+        self.assertEqual(len(user.followers), 0)
+        self.assertEqual(len(user.likes), 0)
+
+    def test_repr_method(self):
+        """Test repr method"""
+
+        user = User(
+            email="test@test.com",
+            username="testuser",
+            password="HASHED_PASSWORD"
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        self.assertEqual(
+            repr(user), f'<User #{user.id}: testuser, test@test.com>'
+        )
+
+    def test_is_following(self):
+        """Test is_following method"""
+
+        user1 = User(
+            email="test@test.com",
+            username="testuser",
+            password="HASHED_PASSWORD"
+        )
+
+        user2 = User(
+            email="test2@testing.com",
+            username="test2user",
+            password="PASSWORD123!"
+        )
+
+        db.session.add(user1)
+        db.session.add(user2)
+        db.session.commit()
+
+        #Create follow relationship
+        follows = Follows(user_being_followed_id=user1.id,
+                          user_following_id=user2.id)
+        
+        db.session.add(follows)
+        db.session.commit()
+
+        #Test if user2 is following user1
+        self.assertEqual(user2.is_following(user1), True)
+        self.assertEqual(len(user2.following), 1)
+
+        #Test if user1 is following user2
+        self.assertEqual(user1.is_following(user2), False)
+        self.assertEqual(len(user2.following), 1)
+
+    def test_is_followed_by(self):
+        """Test is_followed_by method"""
+
+        user1 = User(
+            email="test@test.com",
+            username="testuser",
+            password="HASHED_PASSWORD"
+        )
+
+        user2 = User(
+            email="test2@testing.com",
+            username="test2user",
+            password="PASSWORD123!"
+        )
+
+        db.session.add(user1)
+        db.session.add(user2)
+        db.session.commit()
+
+        #Create follow relationship
+        follows = Follows(user_being_followed_id=user1.id,
+                          user_following_id=user2.id)
+
+        db.session.add(follows)
+        db.session.commit()
+
+        #Test if user1 is followed by user2
+        self.assertEqual(user1.is_followed_by(user2), True)
+        self.assertEqual(len(user1.followers), 1)
+
+        #Test if user2 is followed by user1
+        self.assertEqual(user2.is_followed_by(user1), False)
+        self.assertEqual(len(user2.followers), 0)
+        
+    def test_user_signup(self):
+        """Test creating new user"""
+
+        user1 = User.signup(
+            email="test@test.com",
+            username="testuser",
+            password="HASHED_PASSWORD",
+            image_url=None
+        )
+
+        user2 = User(
+            email="test2@testing.com",
+            username="test2user",
+            password="PASSWORD123!",
+            image_url=None
+        )
+
+        db.session.add(user1)
+        db.session.add(user2)
+        db.session.commit()
+
+        user_count = User.query.count()
+
+        self.assertEqual(user_count, 2)
+
+    
+    def test_user_authenticate(self):
+        """Test user authentication"""
+
+        user = User.signup(
+            email="test@test.com",
+            username="testuser",
+            password="HASHED_PASSWORD",
+            image_url=None
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        auth = User.authenticate("testuser", "HASHED_PASSWORD")
+        invalid = User.authenticate("kenbo", "pa$$word123")
+
+        self.assertTrue(auth)
+        self.assertFalse(invalid)
